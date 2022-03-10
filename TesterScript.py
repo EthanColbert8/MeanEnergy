d51972220b27574cda18c663a1157254d38ec80c
Will Brown <kyrosewoodgang@gmail.com>
2022-03-10 17:44:51 -0500

    Edit script to run properly on Unix-based systems

--- TesterScript.py
+++ TesterScript.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 10 15:22:21 2022
@@ -20,13 +21,13 @@ for line in testList:
     testCases.append(line.split())
 
 for test in range(0, numTests):
-    processResult = subprocess.run(["energy_sequential", testCases[test][0],
+    processResult = subprocess.run(["./energy_sequential", testCases[test][0],
                                     testCases[test][1], testCases[test][2]],
                                    capture_output=True, text=True)
     #print(processResult.stdout)
     obtainedResult = float(processResult.stdout.split()[-1])
     expectedResult = float(testCases[test][3])
-    
+
     comparison = (obtainedResult - expectedResult) / expectedResult
     if comparison < 0.05:
         print("Test", test+1, "passed!")
