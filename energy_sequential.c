3bf205e91e3b976405380d0ce574c076fa146f3c
Will Brown <kyrosewoodgang@gmail.com>
2022-03-09 19:18:13 -0500

    Port energy_sequential to C

---
+++ energy_sequential.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+const double k = 0.0000861733034;
+const double hbar = 6.58211951e-16;
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        printf("Whoops, you need more arguments!\n");
+        return 1;
+    }
+
+    double omega = strtod(argv[1], NULL);
+    double t = strtod(argv[2], NULL);
+    long n = strtol(argv[3], NULL, 10);
+
+    double coeff = -(hbar * omega) / (k * t);
+
+    double topSum = 0.0;
+    double bottomSum = 0.0;
+    double temp;
+
+    for (long count = 0; count <= n; count++) {
+        temp = exp(coeff * count);
+        topSum += (2*count + 1) * temp;
+        bottomSum += temp;
+    }
+
+    double meanEnergy = (hbar / 2) * omega * topSum / bottomSum;
+
+    printf("Mean energy is: %g\n", meanEnergy);
+
+    return 0;
+}
